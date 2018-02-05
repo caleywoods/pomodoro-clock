616405d5a2c7b4159c501f2bce6d8c22a01402e0
Caley Woods <caley.w@gmail.com>
2018-02-04 22:15:03 -0600

    Remove jQuery dependency and begin ES6 update

--- js/index.js
+++ js/index.js
@@ -1,81 +1,84 @@
 $(document).ready(function () {
-  $breakTimer = $('#break-timer');
-  $sessionTimer = $('#session-timer');
-  $breakMinus = $('#break-minus');
-  $breakPlus = $('#break-plus');
-  $sessionMinus = $('#session-minus');
-  $sessionPlus = $('#session-plus');
-  $status = $('#status');
-
-  function decrement(sel,val){
-    sel.html((+val) - 1);
+  const breakTimer   = document.querySelector( '#break-timer' );
+  const sessionTimer = document.querySelector( '#session-timer' );
+  const breakMinus   = document.querySelector( '#break-minus' );
+  const breakPlus    = document.querySelector( '#break-plus' );
+  const sessionMinus = document.querySelector( '#session-minus' );
+  const sessionPlus  = document.querySelector( '#session-plus' );
+  const status       = document.querySelector( '#status' );
+  const el           = document.querySelector( '#timer' );
+  const progress     = document.querySelector( '#progress' );
+  const bStart       = document.querySelector( '#start' );
+  const bPause       = document.querySelector( '#pause' );
+  const bReset       = document.querySelector( '#reset' );
+  
+  const decrement = ( sel, val ) => {
+    sel.html( (+val) - 1 );
   }
 
-  function increment(sel,val){
-    sel.html((+val) + 1);
+  const increment= ( sel, val ) => {
+    sel.html( (+val) + 1 );
   }
-
-  $breakMinus.click(function () {
-    $status.html('Break!');
-    decrement($breakTimer, $breakTimer.html());
-  });
-
-  $breakPlus.click(function () {
-    $status.html('Break!');
-    increment($breakTimer, $breakTimer.html());
-  });
-
-  $sessionMinus.click(function () {
-    $status.html('Session!');
-    decrement($sessionTimer, $sessionTimer.html());
-  });
-
-  $sessionPlus.click(function () {
-    $status.html('Session!');
-    increment($sessionTimer, $sessionTimer.html());
-  });
-
-  function pad(val) {
-    return ('00' + val).slice(-2);
-  }
-
-  var el = document.getElementById('timer');
-
-  function updateDisplay(t) {
-    var hours = Math.floor(t / 3600);
+  
+  const pad = ( val ) => { ('00' + val).slice(-2); }
+  
+  const updateDisplay = ( t ) => {
+    let hours   = Math.floor( t / 3600 );
+    let minutes = Math.floor( t / 60 );
+    let seconds = Math.floor( t );
     t -= hours * 3600;
-    var minutes = Math.floor(t / 60);
     t -= minutes * 60;
-    var seconds = Math.floor(t);
-    el.innerHTML = pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+  
+    el.innerHTML = `${pad(hours)} : ${pad(minutes)} + ${pad(seconds)}`;
   }
+  
+  breakMinus.addEventListener( 'click', () => {
+    status.html( 'Break!' );
+    decrement( breakTimer, breakTimer.html() );
+  });
+  
+  breakPlus.addEventListener( 'click', () => {
+    status.html( 'Break!' );
+    decrement( breakTimer, breakTimer.html() );
+  });
+  
+  sessionMinus.addEventListener( 'click', () => {
+    status.html( 'Session!' );
+    decrement( sessionTimer, sessionTimer.html() );
+  });
+  
+  sessionPlus.addEventListener( 'click', () => {
+    status.html( 'Session!' );
+    decrement( sessionTimer, sessionTimer.html() );
+  });
 
   time = 0;
-  updateDisplay(time);
-  var running = true;
-  var tlast = (new Date()).getTime();
+  updateDisplay( time );
+  let running = true;
+  let tlast = (new Date()).getTime();
 
   function update() {
-    if (time <= 0.0) { // Already done
-      return;
-    }
-    var tnow = (new Date()).getTime();
-    var dt = (tnow - tlast) / 1000.0;
+    //Guard statement to return if already done
+    if time <= 0.0 return;
+    
+    let tnow = (new Date()).getTime();
+    let dt   = (tnow - tlast) / 1000.0;
     tlast = tnow;
     time -= dt;
-    if ($status.html() === 'Session!') {
-      totalTime = ($sessionTimer.html() * 60);
-      water = 'rgba(25, 139, 201, 1)';
+    
+    if ( status.html() === 'Session!' ) {
+      totalTime = ( sessionTimer.html() * 60 );
+      water     = 'rgba(25, 139, 201, 1)';
     }
 
-    if ($status.html() === 'Break!') {
-      totalTime = ($breakTimer.html() * 60);
-      water = 'rgba(255, 0, 0, 1)';
+    if ( status.html() === 'Break!' ) {
+      totalTime = ( breakTimer.html() * 60 );
+      water     = 'rgba(255, 0, 0, 1)';
     }
 
-    fraction = 1 - (time / totalTime);
+    let fraction = 1 - ( time / totalTime );
 
-    $('#progress').waterbubble({
+    progress.waterbubble({
       data: fraction,
       animation: false,
       waterColor: water
@@ -93,44 +96,42 @@ $(document).ready(function () {
 
       }
     }
-    updateDisplay(time);
-    if (running) {
-      requestAnimationFrame(update);
+    
+    updateDisplay( time );
+    
+    if ( running ) {
+      requestAnimationFrame( update );
     }
 
   }
-
-  function run() {
-    $status.html('Session!');
-    if (time <= 0.0) {
-      time = $sessionTimer.html() * 60;
-    }
-    tlast = (new Date()).getTime();
+  
+  const run = () => {
+    status.html( 'Session!' );
+    
+    if ( time <= 0.0 ) { time = sessionTimer.html() * 60; }
+    
+    tlast   = new Date().getTime();
     running = true;
-    requestAnimationFrame(update);
+    requestAnimationFrame( update );
   }
+  
+  const pause = () => { running = false; }
 
-  function pause() {
+  const stop = () => {
     running = false;
-  }
-
-  function stop() {
-    running = false;
-    time = 0;
+    time    = 0;
     el.innerHTML = '00:00:00';
-    $status.html('Session!');
-    $sessionTimer.html(25);
-    $breakTimer.html(5);
-    $('#progress').waterbubble({
+    status.html( 'Session!' );
+    sessionTimer.html( 25 );
+    breakTimer.html( 5 ) ;
+    progress.waterbubble({
       data: 0.0,
       animation: false,
-      waterColor: 'rgba(25, 139, 201, 1)'
+      waterColor: 'rgba(25, 139m 201, 1)'
     });
   }
+  
 
-  var bStart = document.getElementById('start');
-  var bPause = document.getElementById('pause');
-  var bReset = document.getElementById('reset');
 
   bStart.onclick = run;
   bPause.onclick = pause;
